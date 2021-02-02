4512701dba889e96d8a7c0c02b79443f250e7c8f
SY-HHK <louis.ricour@gmail.com>
2021-02-02 11:24:14 +0100

    new initial commit --> react

--- README.md
+++ README.md
@@ -1,11 +1,1 @@
 # TP-IBC-Cryptographie-et-fonction-de-Hashage
-
-## Installation
-
-npm install  
-node index.js (lance le serveur)
-
-## Modification
-
-3 routes disponible dans le index.js  
-3 fichiers html dans templates
---
+++ src/components/App.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react'
+import Navbar from './Navbar'
+import Main from './Main'
+import './App.css'
+
+class App extends Component {
+
+    async componentWillMount() {
+    }
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            loading: false
+        }
+    }
+
+    render() {
+        let content
+        if (this.state.loading) {
+            content = <p id="loader" className="text-center">Loading...</p>
+        } else {
+            content = <Main
+
+            />
+        }
+
+        return (
+            <div>
+                <Navbar/>
+                <div className="container-fluid mt-5">
+                    <div className="row">
+                        <main role="main" className="col-lg-12 ml-auto mr-auto" style={{maxWidth: '600px'}}>
+                            <div className="content mr-auto ml-auto">
+                                <a
+                                    href=""
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                </a>
+
+                                {content}
+
+                            </div>
+                        </main>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
